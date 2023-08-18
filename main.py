d490cdf9d9fe9b229a33675e2824eb1c30ea49b8
BrookAT <142592569+BrookAT@users.noreply.github.com>
2023-08-18 16:06:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ class player():
         self.maxHp = 500
         self.armor = 150
         self.maxEnergy = 100
-        
+        self.maxDamage = 110
+        self.minDamage = 50
     def attack(self):
-        ...
+        self.Phatt = randit(self.minDamage, self.maxDamage)
         
 print('[ ---Welcome to DUNGEN IMPACT--- ]')
 ali = player('ali W')
