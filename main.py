5fbf16e2fef8f244e60000d751a412787bf52d0e
BrookAT <142592569+BrookAT@users.noreply.github.com>
2023-08-18 15:32:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ class player():
         self.armor = 150
         self.maxEnergy = 100
 
-input(f'[ ---Welcome to DUNGEN IMPACT--- ]\n\n1- One')
+input(f'[ ---Welcome to DUNGEN IMPACT--- ]\n\n1- Start...')
 
