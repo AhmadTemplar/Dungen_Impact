635d4f4ac265fcad50a8307b185ffd7f6cb70758
AhmadTemplar <107308542+AhmadTemplar@users.noreply.github.com>
2023-08-18 05:00:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ class player():
         self.armor = 150
         self.maxEnergy = 100
 
-input(f'= = = [ ---Welcome to DUNGEN IMPACT--- ] = = =\nhehe')
-ahmad = player('Master Moonlight')
-print(ahmad.name,ahmad.maxHp,ahmad.armor,ahmad.maxEnergy)
+input(f'= = = [ ---Welcome to DUNGEN IMPACT--- ]\n\n1- One')
+
